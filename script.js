90272d6ced0c7841e2ef99a547c74338b06655e4
Samuelh81 <samuelhornick1@gmail.com>
2023-03-19 23:58:36 -0500

    added error interface

--- script.js
+++ script.js
@@ -6,7 +6,8 @@ async function updateData() {
     const result = await fetch(getApiLink(playerName));
     jsonData = await result.json();
     if (jsonData['code'] != 200) {
-        alert("API Error "+jsonData['code']);
+        alert("API Error "+ getErrorMessage(jsonData['code']));
+        console.error("API Error "+ getErrorMessage(jsonData['code']));
         return;
     } 
 
@@ -40,7 +41,6 @@ async function updateData() {
     document.getElementById("playerName").innerHTML = playerName;
     document.getElementById("rank").innerHTML = rank;
 
-
 }
 updateData();
 
@@ -48,6 +48,32 @@ function getApiLink(name) {
     return "https://api.wynncraft.com/v2/player/"+name+"/stats";
 }
 
-
+function getErrorMessage(errorCode) {
+    var message;
+    switch (errorCode) {
+        case 400:
+            message = "400 - Bad Request";
+            break;
+        case 404:
+            message = "404 - Not Found";
+            break;
+        case 429:
+            message = "429 - Too Many Requests";
+            break;
+        case 500:
+            message = "500 - Internal Server Error";
+            break;
+        case 502:
+            message = "502 - Bad Gateway";
+            break;
+        case 502:
+            message = "503 - Service Unavailable";
+            break;
+        default:
+            message = "Unknown Error"
+            break;
+    }
+    return message; 
+}
 
 // document.getElementById("button").addEventListener('click', event => {cycleWallpaper();});
